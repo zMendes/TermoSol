387f16481b22fd5be3e9dcd4bbd86fcde4b8c66b
zMendes <leonardom1808@gmail.com>
2020-03-13 15:40:06 -0300

    Ok calculei o U certo mas agr n sei como fazer pra identificar quais que foram calculados.

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ from ponte import Ponte
 
 ##Pegando dados da tabela
 [n_nos, nos_coord, n_elements, param , n_force, force_matrix, n_restric, restric_matrix] = importa('entrada.xlsx')
-#print(param)
 element = []
 nodes = []
-U = [0,0,0]
-force_matrix = force_matrix[3:6]
+dict_nos = {}
+U = np.array([n_elements*2])
+
 for i in param:
     nodes.append(np.array([int(i[0]),int(i[1])]))
 # print(nodes)
 
-dict_nos = {}
 
 for i in range(0,n_nos):
     dict_nos[i+1] = [nos_coord[0][i], nos_coord[1][i]]
@@ -25,7 +24,7 @@ for i in range(0,n_nos):
     element.append(Element(dict_nos.get(int(param[i][0])),dict_nos.get(int(param[i][1])),param[i][3],param[i][2],nodes[i]))
 
 
-ponte = Ponte(element,force_matrix,U,n_nos)
+ponte = Ponte(element,force_matrix,U,n_nos,restric_matrix)
 ponte.getKis()
 ponte.getKg()
-print(ponte.Kg)
+ponte.condicContorno()
--- ponte.py
+++ ponte.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 class Ponte():
-    def __init__(self, elements, F, U, n_nos):
+    def __init__(self, elements, F, U, n_nos,restric_matrix):
         self.elements = elements
         self.F = F
         self.U = U
         self.n_nos = n_nos
+        self.restric_matrix = restric_matrix
         self.Kg = np.zeros([n_nos*2,n_nos*2])
 
     def getKis(self):
@@ -17,7 +18,15 @@ class Ponte():
             self.Kg[element.gdl[0]:element.gdl[1]+1,element.gdl[0]:element.gdl[1]+1] += element.K[0:2,0:2]
             self.Kg[element.gdl[2]:element.gdl[3]+1,element.gdl[2]:element.gdl[3]+1] += element.K[2:4,2:4] 
             self.Kg[element.gdl[0]:element.gdl[1]+1,element.gdl[2]:element.gdl[3]+1] += element.K[0:2,2:4] 
-            self.Kg[element.gdl[2]:element.gdl[3]+1,element.gdl[0]:element.gdl[1]+1] += element.K[2:4,0:2] 
+            self.Kg[element.gdl[2]:element.gdl[3]+1,element.gdl[0]:element.gdl[1]+1] += element.K[2:4,0:2]
 
+    def condicContorno(self):
+            self.Kg = np.delete(self.Kg,self.restric_matrix,0)
+            self.Kg = np.delete(self.Kg,self.restric_matrix,1)
+            self.F = np.delete(self.F, self.restric_matrix)
+            # self.U = np.linalg.inv(self.Kg) * self.F
+            self.U = np.linalg.inv(self.Kg).dot( self.F)
+    
+            
 
 
